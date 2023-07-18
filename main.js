002af186404d2a6c86a31e8a00e2e9cee9f951db
Karina Joanna <kxochipam2000@alumno.ipn.mx>
2023-07-17 21:48:02 -0600

    visual changes

--- main.js
+++ main.js
@@ -7,7 +7,7 @@ let typewriter = new Typewriter(app, {
  
 typewriter
   .pauseFor(2500)
-  .typeString('Desarrollo sitios web y escribo artículos sobre programación.')
+  .typeString('Part Time Programmer and Front - End developer.')
   .pauseFor(200)
   .deleteChars(10)
   .start();
